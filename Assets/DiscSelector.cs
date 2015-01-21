3b07d440c09709a523136806996217ef3fefbdfa
Chris Zinkula <git@chriszinkula.com>
2015-01-20 20:02:38 -0600

    Initial checkin

---
+++ Assets/DiscSelector.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiscSelector : MonoBehaviour {
+	public float pulseTime = 1.0f;
+
+	private SpriteRenderer discSprite;
+	private float nextPulse;
+	private int pulseDirection = -1;
+	private bool clicked = false;
+
+	// Use this for initialization
+	void Awake () {
+		discSprite = transform.GetComponent<SpriteRenderer>();
+		nextPulse = Time.time + pulseTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (clicked) {
+			float r = discSprite.color.r;
+			float timeDiff = 1.0f * pulseDirection * Time.deltaTime / pulseTime;
+			r = r + timeDiff;
+
+			if (Time.time >= nextPulse) {
+				if (pulseDirection == -1) {
+					r = 0.0f;
+				} else {
+					r = 1.0f;
+				}
+
+				nextPulse = Time.time + pulseTime;
+				pulseDirection *= -1;
+			}
+
+			discSprite.color = new Color(r, 1.0f, 1.0f);
+		}
+	}
+
+	void OnMouseDown() {
+		clicked = !clicked;
+
+		Debug.Log (transform.name + " was clicked.");
+		if (clicked) {
+			nextPulse = Time.time + pulseTime;
+		} else {
+			discSprite.color = Color.white;
+		}
+	}
+}
---
+++ Assets/SpawnDiscs.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnDiscs : MonoBehaviour {
+
+	public Transform disc;
+	public Transform startingPost;
+	public float discReduction = 0.15f;
+
+	private Transform largeDisc;
+	private Transform mediumDisc;
+	private Transform smallDisc;
+
+	// Use this for initialization
+	void Awake () {
+		if (disc != null) {
+			float postHeight = startingPost.GetComponent<SpriteRenderer>().bounds.size.y;
+			float discHeight = disc.GetComponent<SpriteRenderer>().bounds.size.y;
+			// todo consider scale with collider
+			largeDisc = Instantiate(disc, startingPost.position - new Vector3(0, postHeight / 2 - discHeight / 2, 0), startingPost.rotation) as Transform;
+			largeDisc.name = "Large Disc";
+			BoxCollider2D collider = largeDisc.GetComponent<BoxCollider2D>();
+			collider.size = largeDisc.GetComponent<SpriteRenderer>().bounds.size;
+
+			mediumDisc = Instantiate(disc, largeDisc.position + new Vector3(0, discHeight, 0), largeDisc.rotation) as Transform;
+			mediumDisc.localScale = largeDisc.localScale - new Vector3(discReduction, 0f, 0f);
+			mediumDisc.name = "Medium Disc";
+			collider = mediumDisc.GetComponent<BoxCollider2D>();
+			collider.size = mediumDisc.GetComponent<SpriteRenderer>().bounds.size;
+
+
+			smallDisc = Instantiate(disc, mediumDisc.position + new Vector3(0, discHeight, 0), mediumDisc.rotation) as Transform;
+			smallDisc.localScale = mediumDisc.localScale - new Vector3(discReduction, 0f, 0f);
+			smallDisc.name = "Small Disc";
+			collider = smallDisc.GetComponent<BoxCollider2D>();
+			collider.size = smallDisc.GetComponent<SpriteRenderer>().bounds.size;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

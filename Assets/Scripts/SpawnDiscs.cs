847e5612648f676fe72ee7eb822c6bcefdcff9d4
Chris Zinkula <git@chriszinkula.com>
2015-01-21 22:41:31 -0600

    Did not need to scale boxcolliders.  I have no idea how I got down that path.  Fixed by not trying to scale them.

--- Assets/Scripts/SpawnDiscs.cs
+++ Assets/Scripts/SpawnDiscs.cs
@@ -16,24 +16,17 @@ public class SpawnDiscs : MonoBehaviour {
 		if (disc != null) {
 			float postHeight = startingPost.GetComponent<SpriteRenderer>().bounds.size.y;
 			float discHeight = disc.GetComponent<SpriteRenderer>().bounds.size.y;
-			// todo consider scale with collider
 			largeDisc = Instantiate(disc, startingPost.position - new Vector3(0, postHeight / 2 - discHeight / 2, 0), startingPost.rotation) as Transform;
 			largeDisc.name = "Large Disc";
-			BoxCollider2D collider = largeDisc.GetComponent<BoxCollider2D>();
-			collider.size = largeDisc.GetComponent<SpriteRenderer>().bounds.size;
 
 			mediumDisc = Instantiate(disc, largeDisc.position + new Vector3(0, discHeight, 0), largeDisc.rotation) as Transform;
 			mediumDisc.localScale = largeDisc.localScale - new Vector3(discReduction, 0f, 0f);
 			mediumDisc.name = "Medium Disc";
-			collider = mediumDisc.GetComponent<BoxCollider2D>();
-			collider.size = mediumDisc.GetComponent<SpriteRenderer>().bounds.size;
 
 
 			smallDisc = Instantiate(disc, mediumDisc.position + new Vector3(0, discHeight, 0), mediumDisc.rotation) as Transform;
 			smallDisc.localScale = mediumDisc.localScale - new Vector3(discReduction, 0f, 0f);
 			smallDisc.name = "Small Disc";
-			collider = smallDisc.GetComponent<BoxCollider2D>();
-			collider.size = smallDisc.GetComponent<SpriteRenderer>().bounds.size;
 		}
 	}
 	

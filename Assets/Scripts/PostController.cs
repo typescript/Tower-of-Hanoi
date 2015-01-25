9160d0880a861832603f0414afd1fc5a7a6dbfe2
Chris Zinkula <git@chriszinkula.com>
2015-01-24 21:18:33 -0600

    Got the discs and posts talking to each other.  Not sure I care for the implementation but we'll always improve as we learn.

---
+++ Assets/Scripts/PostController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostController : MonoBehaviour {
+    private GameObject selectedPost = null;
+    private GameController gameController;
+    private DiscController discController;
+
+	// Use this for initialization
+	void Start () {
+        gameController = this.GetComponentInParent<GameController>();
+        discController = transform.parent.GetComponentInChildren<DiscController>();
+	}
+
+    public bool isPostSelected()
+    {
+        return this.selectedPost != null;
+    }
+
+    public void setSelectedPost(GameObject selectedPost)
+    {
+        if (discController.isDiscSelected()) {
+            if (selectedPost != null)
+            {
+                Debug.Log("The following post was selected: " + selectedPost.name);
+            }
+            else
+            {
+                Debug.Log("No post is selected.");
+            }
+            this.selectedPost = selectedPost;
+        }
+        else
+        {
+            Debug.Log("Cannot select post until disc is selected.");
+        }
+    }
+
+    public GameObject getSelectedPost()
+    {
+        return this.selectedPost;
+    }
+}
---
+++ Assets/Scripts/PostSelector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostSelector : MonoBehaviour {
+    private PostController postController;
+
+	// Use this for initialization
+	void Start () {
+        postController = this.GetComponentInParent<PostController>();
+	}
+
+    void OnMouseDown()
+    {
+        if (!postController.isPostSelected())
+        {
+            postController.setSelectedPost(this.gameObject);
+        }
+    }
+}
